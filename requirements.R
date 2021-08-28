a6d57b9dec63b4c51d61eae16f2ef1032c136b1b
Alice Lepissier <alice.lepissier@gmail.com>
2021-08-28 01:09:35 +0000

    Add requirements.R

---
+++ requirements.R
@@ -0,0 +1,12 @@
+# IllicitAI
+# Alice Lepissier
+# alice.lepissier@gmail.com
+
+## ## ## ## ## ## ## ## ## ## ##
+# REQUIREMENTS              ####
+## ## ## ## ## ## ## ## ## ## ##
+
+Sys.setenv(LIBARROW_BINARY = TRUE)
+install.packages("arrow", 
+                 repos = "http://cran.us.r-project.org",
+                 type = "source")
